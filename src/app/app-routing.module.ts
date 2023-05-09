e823eef724f037db82b5c85c89014852c4f36319
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-08 21:05:55 -0600

    Add - Error page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './home/home.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
 
+  {path: 'error', loadChildren: () => import('./error-page/error-page.module').then( m => m.ErrorPageModule)},
   {path: '**', component: HomeComponent},
 ];
 
