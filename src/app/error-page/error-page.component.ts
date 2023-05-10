b8ba74c50626229d641437772aca2c2a579a9320
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-10 01:39:47 -0600

    Add - error page + styles

--- src/app/error-page/error-page.component.ts
+++ src/app/error-page/error-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-error-page',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { 
+    /* this.renderer.addClass(document.body, "testeoclass"); */
+  }
 
   ngOnInit(): void {
+    const body = this.renderer.parentNode(this.renderer.parentNode(this.elementRef.nativeElement));
+    console.log(body);
+    this.renderer.addClass(body, "error-page-bg");
   }
 
 }
