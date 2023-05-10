808b5c8614a55336e39d47db0e4cfebc9124c628
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-10 02:20:43 -0600

    Add - Take a Pill function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
+  }
+
+  takeAPill() {
+    const pill = this.elementRef.nativeElement.querySelector('#pill');
+    this.renderer.removeClass(pill, "pill-normal");
+    this.renderer.addClass(pill, "pill-glitched");
   }
 
 }
