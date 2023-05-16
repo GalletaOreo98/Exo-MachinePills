00da90c78527f703cfe1aa0351048119cf73b873
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-16 02:45:21 -0600

    Add - Play sound in hidden layer

--- src/app/layers/crypt/crypt.component.ts
+++ src/app/layers/crypt/crypt.component.ts
@@ -7,17 +7,23 @@ import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 })
 export class CryptComponent implements OnInit {
 
+  private audio: HTMLAudioElement;
   body: any;
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
+  constructor(private renderer: Renderer2, private elementRef: ElementRef) { 
+    this.audio = new Audio();
+    this.audio.src = '/assets/audio/creepy-cavern-ambience.mp3';
+  }
 
   ngOnInit(): void {
     this.body = this.renderer.parentNode(this.renderer.parentNode(this.elementRef.nativeElement));
     this.renderer.addClass(this.body, "error-page-bg");
+    this.audio.play();
   }
 
   ngOnDestroy(): void {
     this.renderer.removeClass(this.body, "error-page-bg");
+    this.audio.pause();
   }
 
 }
