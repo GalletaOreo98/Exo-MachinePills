203e198a7bdc72ba77f72432980d76e78b2631b7
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-20 20:57:10 -0600

    Add - Custom Lazy Load to carousel in ai submodule

--- src/app/layers/immersion/modules/ai/carousel/carousel.component.ts
+++ src/app/layers/immersion/modules/ai/carousel/carousel.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbSlideEvent } from '@ng-bootstrap/ng-bootstrap';
 
 interface carouselImage {
   src: string,
-  date: string
+  //date: string
 }
 
 @Component({
@@ -12,14 +13,26 @@ interface carouselImage {
 })
 export class CarouselComponent implements OnInit {
 
-  images:carouselImage[] = [
-    {src: "assets/images/layers/immersion/ia/0.jpg", date: "19/05/23"},
-    {src: "assets/images/layers/immersion/ia/1.jpg", date: "19/05/23"}
-  ]
+  images: carouselImage[] = [
+    { src: "assets/images/layers/immersion/ia/0.jpg"},
+    { src: "assets/images/layers/immersion/ia/1.jpg"}
+  ];
+
+  lastImage = 1;
+  totalImages = 5;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onSlideChange(event: NgbSlideEvent) {
+    const activeSlide = Number(event.current.slice(10));
+    if(this.lastImage != activeSlide || this.lastImage === this.totalImages - 1) return;
+    console.log("long: " + this.images.length);
+    console.log("actual: " + activeSlide);
+    console.log("lastimage: " + this.lastImage);
+    this.images.push({ src: `assets/images/layers/immersion/ia/${(activeSlide+1)}.jpg`});
+    this.lastImage++;
+  }
 }
