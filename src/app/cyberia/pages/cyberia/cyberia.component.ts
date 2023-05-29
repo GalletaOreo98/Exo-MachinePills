4bb03e522636652f23d9367c70dd7c9d08d6824e
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-05-29 01:01:50 -0600

    Add - Botones de funcionalidad cyberia layer

--- src/app/cyberia/pages/cyberia/cyberia.component.ts
+++ src/app/cyberia/pages/cyberia/cyberia.component.ts
@@ -20,10 +20,13 @@ export class CyberiaComponent implements OnInit, AfterViewInit {
   lain_left_2 = new Image();
   tv = new Image();
   readonly CANVAS_SUELO = 472;
+  readonly INITIAL_CHARACTER_X = 180;
   character = {
-    x: 180,
+    x: this.INITIAL_CHARACTER_X,
     y: this.CANVAS_SUELO,
-    animationTime: 0
+    animationTime: 0,
+    estado: "NORMAL",
+    isMoving: false
   };
   tvObject = {
     x: 110,
@@ -106,6 +109,111 @@ export class CyberiaComponent implements OnInit, AfterViewInit {
     this.character.y = this.character.y * 0.5;
     this.tvObject.x = this.tvObject.x * 0.5;
     this.tvObject.y = this.tvObject.y * 0.5;
+  }
+
+  //GAME FUNCTIONS
+
+  private updateCharacterAnimation(action: string): void {
+    switch (action) {
+      case 'MOV_DERECHA':
+        this.character.x = this.character.x + 3;
+        this.character.animationTime++;
+
+        if (this.character.animationTime == 1) {
+          this.character.estado = 'MOV_DERECHA';
+        }
+        if (this.character.animationTime == 6) {
+          this.character.estado = 'NORMAL';
+          this.character.animationTime = -2;
+        }
+        break;
+      case 'MOV_IZQUIERDA':
+
+        this.character.x = this.character.x - 3;
+        this.character.animationTime++;
+
+        if (this.character.animationTime == 1) {
+          this.character.estado = 'MOV_IZQUIERDA';
+        }
+        if (this.character.animationTime == 6) {
+          this.character.estado = 'NORMAL2';
+          this.character.animationTime = -2;
+        }
+        break;
+      case 'STOP':
+        switch (this.character.estado) {
+          case 'MOV_DERECHA':
+            this.character.estado = 'NORMAL'
+            break;
+          case 'MOV_IZQUIERDA':
+            this.character.estado = 'NORMAL2'
+            break;
+          default:
+            break;
+        }
+        this.character.animationTime = 0;
+        break;
+      default:
+        break;
+    }
+  }
+
+  private drawCharacter(): void {
+    switch (this.character.estado) {
+      case 'NORMAL':
+          this.ctx!.drawImage(this.lain_right_1, 0 , 0, this.lain_right_1.width, this.lain_right_1.height, this.character.x, this.character.y, this.lain_right_1.width, this.lain_right_1.height);
+          break;
+      case 'MOV_DERECHA':
+          this.ctx!.drawImage(this.lain_right_2, 0 , 0, this.lain_right_1.width, this.lain_right_1.height, this.character.x, this.character.y, this.lain_right_1.width, this.lain_right_1.height);
+          break;
+      case 'NORMAL2':
+          this.ctx!.drawImage(this.lain_left_1, 0 , 0, this.lain_right_1.width, this.lain_right_1.height, this.character.x, this.character.y, this.lain_right_1.width, this.lain_right_1.height);
+          break;
+      case 'MOV_IZQUIERDA':
+          this.ctx!.drawImage(this.lain_left_2, 0 , 0, this.lain_right_1.width, this.lain_right_1.height, this.character.x, this.character.y, this.lain_right_1.width, this.lain_right_1.height);
+          break;
+      default:
+          break;
+  }
+  }
+
+  private drawCanvasBackground(): void {
+    this.ctx!.drawImage(this.background, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.ctx!.drawImage(this.tv, this.tvObject.x, this.tvObject.y, this.tv.width, this.tv.height);
+  }
+
+  private clearCanvas(): void {
+    this.ctx!.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+  }
+
+  play(action: string): void {
+    if(this.character.isMoving) return;
+    if(this.character.x > this.INITIAL_CHARACTER_X && action == "MOV_DERECHA") return;
+    if(this.character.x <= this.INITIAL_CHARACTER_X && action == "MOV_IZQUIERDA") return;
+    this.character.isMoving = true;
+    this.slowLoopExecution(action);
+  }
+
+  private stopCharachter(): void {
+    this.updateCharacterAnimation("STOP");
+    this.drawCharacter();
+  }
+
+  private async slowLoopExecution(action: string): Promise<void> {
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+    const totalIterations = 60; //Practicamente es la distancia que se va a mover
+  
+    for (let i = 0; i < totalIterations; i++) {
+      // Codigo que deseas ejecutar en cada iteracion del bucle
+      this.clearCanvas();
+      this.drawCanvasBackground();
+      this.updateCharacterAnimation(action);
+      this.drawCharacter();
+      await delay(85); // Tiempo entre animaciones en milisegundos
+    }
+    this.stopCharachter();
+    this.character.isMoving = false;
   }
 
 }
