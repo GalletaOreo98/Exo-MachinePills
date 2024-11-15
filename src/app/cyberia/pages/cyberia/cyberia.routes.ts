84a9e6e7a6b04e4135bca11cf65786ecb8bb7f9e
GalletaOreo98 <christophergironcabrera@gmail.com>
2024-11-15 00:57:08 -0600

    FIX - Minimal fix

--- src/app/cyberia/pages/cyberia/cyberia.routes.ts
+++ src/app/cyberia/pages/cyberia/cyberia.routes.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
   },
   {
     path: 'laintest',
-    loadComponent: () => import('../laintest/laintest.component').then(m => m.LaintestComponent),
+    loadComponent: () => import('../laintest/laintest.component').then(c => c.LaintestComponent),
   }
 ];
 export default routes;
