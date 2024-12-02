0bd2911f433930b0b2a885d7fc6b1c62f697be7f
GalletaOreo98 <christophergironcabrera@gmail.com>
2024-12-01 20:55:20 -0600

    Add - Stats bars to laintest result

--- src/app/cyberia/pages/laintest/laintest.component.ts
+++ src/app/cyberia/pages/laintest/laintest.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { LaintestService } from './laintest.service'; // Asegúrate de importar el servicio
 
 export interface Question {
@@ -17,12 +17,15 @@ export interface Option {
   templateUrl: './laintest.component.html',
   styleUrl: './laintest.component.css'
 })
-export class LaintestComponent implements OnInit{
+export class LaintestComponent implements OnInit {
   private audio: HTMLAudioElement;
   @ViewChild('novelContainer') novelContainer!: ElementRef;
 
 
   questions: Question[] = [];
+
+  MAX_POINTS_PER_PERSONALITY = 11; // Número maximo de puntos que se pueden obtener por personalidad
+  MAX_POINTS_LGL = 3; // Número total de puntos que se pueden obtener para 08-lain-glitch (LGL)
 
   personalityPoints: { [key: string]: number } = {
     LB: 0,//LAIN_BRAVA 
@@ -46,7 +49,7 @@ export class LaintestComponent implements OnInit{
     LN: 'assets/images/lain-test/lain-cards/06-lain-narcisista.png',
     LGL: 'assets/images/lain-test/lain-cards/08-lain-glitch.png'
   };
-  
+
   testResultImage: string = '';
   finished = false;
   questionsLoaded = false;
@@ -71,15 +74,20 @@ export class LaintestComponent implements OnInit{
   displayedText = '(Toca para continuar)';
   charIndex = 0;
   typingSpeed = 30; // Velocidad de escritura (milisegundos)
+  
+  //Stats bars
+  vitality = 100;
+  strength = 80;      
+  energy = 60;    
+  intelligence = 90;
 
-  constructor(private questionService: LaintestService, private renderer: Renderer2) { 
+  constructor(private questionService: LaintestService, private renderer: Renderer2) {
     this.audio = new Audio();
     this.audio.src = 'assets/audio/lain-test/blip.mp3';
   }
 
   ngOnInit(): void {
-    //this.typeText();
-    this.loadQuestions();    
+    this.loadQuestions();
   }
 
   ngOnDestroy(): void {
@@ -92,7 +100,7 @@ export class LaintestComponent implements OnInit{
       this.questionsLoaded = true;
       console.log(this.personalityPoints);
       console.log("Total preguntas:", this.questions.length);
-    });    
+    });
   }
 
   selectOption(option: any): void {
@@ -107,13 +115,14 @@ export class LaintestComponent implements OnInit{
       this.currentQuestionIndex++;
     } else {
       this.testResult = this.getResult();
+      this.calculateStatsBar();
       this.finished = true;
     }
   }
 
   getResult(): string {
     let maxPoints = -1;
-    let topPersonality:string = '';
+    let topPersonality: string = '';
 
     for (let personality in this.personalityPoints) {
       if (this.personalityPoints[personality] > maxPoints) {
@@ -144,49 +153,82 @@ export class LaintestComponent implements OnInit{
       default:
         break;
     }
-    
     return topPersonality;
   }
-  
-    // Función para simular el efecto de máquina de escribir
-    typeText() {
-      if (this.charIndex < this.fullText.length) {
-        this.displayedText += this.fullText.charAt(this.charIndex);
-        this.charIndex++;
-        this.playBlipSound();
-        setTimeout(() => this.typeText(), this.typingSpeed);
-      }
-    }
-  
-    advanceText() {
-      if (this.charIndex < this.fullText.length) {
-        // Si el texto aún no está completo, lo muestra instantáneamente
-        this.displayedText = this.fullText;
-        this.charIndex = this.fullText.length;
-      } else if (this.currentTextIndex < this.texts.length - 1) {
-        // Avanza al siguiente bloque de texto
-        this.currentTextIndex++;
-        this.fullText = this.texts[this.currentTextIndex];
-        this.displayedText = '';
-        this.charIndex = 0;
-        this.typeText();
-      } else {
-        console.log('Fin del diálogo.');
-        this.fadeOutNovel();
-        setTimeout(() => this.isDialoguing = false, 1100);
-      }
-    }
 
-    skipDialogue() {
+  calculateStatsBar() {
+    //Vitalidad: LO, LV
+    const totalVitality = this.personalityPoints['LO'] + this.personalityPoints['LV'];
+    console.log('Vitalidad:', totalVitality);
+    const vitalityPercentage = (totalVitality / (this.MAX_POINTS_PER_PERSONALITY)) * 100;
+    console.log('Vitalidad (%):', vitalityPercentage);
+    this.vitality = vitalityPercentage;
+    //Fuerza: LB, LN
+    const totalStrength = this.personalityPoints['LB'] + this.personalityPoints['LN'];
+    console.log('Fuerza:', totalStrength);
+    const strengthPercentage = (totalStrength / (this.MAX_POINTS_PER_PERSONALITY)) * 100;
+    console.log('Fuerza (%):', strengthPercentage);
+    this.strength = strengthPercentage;
+    //Energia: LT, LD
+    let totalEnergy;
+    totalEnergy = this.MAX_POINTS_PER_PERSONALITY - this.personalityPoints['LD'];
+    this.personalityPoints['LD'] == 5 ? totalEnergy -= 2 : totalEnergy;
+    this.personalityPoints['LD'] >= 6 ? totalEnergy = 0 : totalEnergy;
+    (this.personalityPoints['LT'] >= 3 && this.personalityPoints['LD'] > 3) ? totalEnergy++ : totalEnergy;
+    this.personalityPoints['LT'] >= 5 ? totalEnergy++ : totalEnergy;
+    totalEnergy = totalEnergy < 0 ? 0 : totalEnergy;
+    console.log('Energia:', totalEnergy);
+    const energyPercentage = (totalEnergy / this.MAX_POINTS_PER_PERSONALITY) * 100;
+    console.log('Energia (%):', energyPercentage);
+    this.energy = energyPercentage;
+    //Inteligencia: LG, LGL
+    const totalIntelligence = this.personalityPoints['LG'] + this.personalityPoints['LGL'];
+    console.log('Inteligencia:', totalIntelligence);
+    const intelligencePercentage = (totalIntelligence / ((this.MAX_POINTS_PER_PERSONALITY + this.MAX_POINTS_LGL)/1.2)) * 100;
+    console.log('Inteligencia (%):', intelligencePercentage);
+    this.intelligence = intelligencePercentage;
+  }
+  
+
+  // Función para simular el efecto de máquina de escribir
+  typeText() {
+    if (this.charIndex < this.fullText.length) {
+      this.displayedText += this.fullText.charAt(this.charIndex);
+      this.charIndex++;
+      this.playBlipSound();
+      setTimeout(() => this.typeText(), this.typingSpeed);
+    }
+  }
+
+  advanceText() {
+    if (this.charIndex < this.fullText.length) {
+      // Si el texto aún no está completo, lo muestra instantáneamente
+      this.displayedText = this.fullText;
+      this.charIndex = this.fullText.length;
+    } else if (this.currentTextIndex < this.texts.length - 1) {
+      // Avanza al siguiente bloque de texto
+      this.currentTextIndex++;
+      this.fullText = this.texts[this.currentTextIndex];
+      this.displayedText = '';
+      this.charIndex = 0;
+      this.typeText();
+    } else {
       console.log('Fin del diálogo.');
-      this.isDialoguing = false;
+      this.fadeOutNovel();
+      setTimeout(() => this.isDialoguing = false, 1100);
     }
-  
-    playBlipSound() {
-      this.audio.play();
-    }
+  }
 
-    fadeOutNovel() {
-      this.renderer.addClass(this.novelContainer.nativeElement, 'fade-out');
-    }
+  skipDialogue() {
+    console.log('Fin del diálogo.');
+    this.isDialoguing = false;
+  }
+
+  playBlipSound() {
+    this.audio.play();
+  }
+
+  fadeOutNovel() {
+    this.renderer.addClass(this.novelContainer.nativeElement, 'fade-out');
+  }
 }
