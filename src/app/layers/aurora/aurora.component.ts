ab88f18d275aeeaa353c74f188b0804a1e33366e
GalletaOreo98 <christophergironcabrera@gmail.com>
2025-07-21 10:47:25 -0600

    UPDATE: Play meow cat sound is now preloaded and stored on a variable (not recreated every time u play it)

--- src/app/layers/aurora/aurora.component.ts
+++ src/app/layers/aurora/aurora.component.ts
@@ -23,30 +23,33 @@ export class AuroraComponent {
   layerBodyData = layerBodyData;
   private audio: HTMLAudioElement;
 
-  constructor(private title:Title, private meta:Meta) {
+  constructor(private title: Title, private meta: Meta) {
     this.title.setTitle(this.layerMainData.layerName);
 
-    this.meta.updateTag({name: 'description', content: "Let's color all the ways."});
-    this.meta.updateTag({name: 'keywords', content: 'aurora, fan art lain iwakura, cyberpunk, Cyberia club, club, Exo-Machine, Exo, Machine, Exo-Machine Pills, serial experiments lain, or3odev, lain chikita, lain iwakura'});
-    this.meta.updateTag({name: 'author', content: 'GalletaOreo98'});
+    this.meta.updateTag({ name: 'description', content: "Let's color all the ways." });
+    this.meta.updateTag({ name: 'keywords', content: 'aurora, fan art lain iwakura, cyberpunk, Cyberia club, club, Exo-Machine, Exo, Machine, Exo-Machine Pills, serial experiments lain, or3odev, lain chikita, lain iwakura' });
+    this.meta.updateTag({ name: 'author', content: 'GalletaOreo98' });
 
-    this.meta.updateTag({property: 'og:title', content: 'Aurora'});
-    this.meta.updateTag({property: 'og:image', content: 'https://exo-machinepills.com/assets/images/layers/aurora/main.jpg'});
+    this.meta.updateTag({ property: 'og:title', content: 'Aurora' });
+    this.meta.updateTag({ property: 'og:image', content: 'https://exo-machinepills.com/assets/images/layers/aurora/main.jpg' });
 
-    this.meta.updateTag({name: 'twitter:card', content: 'summary_large_image'});
-    this.meta.updateTag({name: 'twitter:title', content: 'Aurora'});
-    this.meta.updateTag({name: 'twitter:description', content: "Let's color all the ways."});
-    this.meta.updateTag({name: 'twitter:image', content: 'https://exo-machinepills.com/assets/images/layers/aurora/main.jpg'});
+    this.meta.updateTag({ name: 'twitter:card', content: 'summary_large_image' });
+    this.meta.updateTag({ name: 'twitter:title', content: 'Aurora' });
+    this.meta.updateTag({ name: 'twitter:description', content: "Let's color all the ways." });
+    this.meta.updateTag({ name: 'twitter:image', content: 'https://exo-machinepills.com/assets/images/layers/aurora/main.jpg' });
 
     this.audio = new Audio();
+    this.audio.src = '/assets/audio/meow-cat.mp3';
   }
 
-  handleImageCatClick(){
-    this.audio.src = '/assets/audio/meow-cat.mp3';
+  handleImageCatClick() {
+    if (!this.audio) this.audio = new Audio('/assets/audio/meow-cat.mp3');
+    this.audio.pause();
+    this.audio.currentTime = 0;
     this.audio.play();
   }
 
-  handleImageXmasClick(){
+  handleImageXmasClick() {
     this.audio.src = '/assets/audio/short-8bits-xmas-song.mp3';
     this.audio.play();
     this.printConfetti();
